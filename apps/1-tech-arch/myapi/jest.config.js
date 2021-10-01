d4ff901120ff199c91a909ecda745342434f88c7
David <dukcho86@gmail.com>
2021-10-01 10:57:27 -0400

    nx-serverless installed

---
+++ apps/1-tech-arch/myapi/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: '1-tech-arch-myapi',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/1-tech-arch/myapi',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/1-tech-arch/myapi'],
+};
