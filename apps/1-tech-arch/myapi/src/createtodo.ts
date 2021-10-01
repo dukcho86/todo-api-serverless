5ea57c0e97c825fd891e40670c91d3b118a77a94
David <dukcho86@gmail.com>
2021-10-01 15:01:56 -0400

    sls dynamodb install

---
+++ apps/1-tech-arch/myapi/src/createtodo.ts
@@ -0,0 +1,52 @@
+export interface TodoItem {
+  id: string;
+  name: string;
+  done: boolean;
+  createdAt: string;
+}
+
+import * as AWS from 'aws-sdk';
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as uuid from 'uuid';
+// import { TodoItem } from '../../models/TodoItem';
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const { name } = JSON.parse(event.body);
+
+  const id = uuid.v4();
+  const todo: TodoItem = {
+    id,
+    done: false,
+    createdAt: new Date().toISOString(),
+    name,
+  };
+
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  await docClient
+    .put({
+      TableName: process.env.TODOS_TABLE,
+      Item: todo,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      item: todo,
+    }),
+  };
+};
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    return new AWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000',
+    });
+  }
+
+  return new AWS.DynamoDB.DocumentClient();
+}
